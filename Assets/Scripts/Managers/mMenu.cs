bb2d8701bd4fad3c66421405e0c80831a5d725fc
Robb <robb.navhrod@gmail.com>
2022-02-12 19:11:47 +0100

    MainMenu, part 2

---
+++ Assets/Scripts/Managers/mMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class mMenu : MonoBehaviour
+{
+    [Header("Objetos del menu")]
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject optionsMenu;
+
+    private void Start()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void loadOptionMenu()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+
+    }
+    public void loadMainMenu()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+
+    }
+
+    public void startNewGame()
+    {
+        StartCoroutine(loadNewGame());
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+
+    private IEnumerator loadNewGame()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("Level1");
+    }
+}
